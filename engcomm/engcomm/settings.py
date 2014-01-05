7de79b336d692e6ea800665ae04066ca1cced2a1
C-o-r-E <can.of.tuna@gmail.com>
2014-01-05 01:33:35 -0500

    added automatic team score calculation, fixed teams with whitespace

--- engcomm/engcomm/settings.py
+++ engcomm/engcomm/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
     'scoreboard',
 )
 
@@ -58,6 +59,15 @@ WSGI_APPLICATION = 'engcomm.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecccdb',
+        'USER': 'django_user',
+        'PASSWORD': 'django123',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    },
+
+    'old': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
--- engcomm/scoreboard/models.py
+++ engcomm/scoreboard/models.py
@@ -1,3 +1,4 @@
+from re import sub
 from django.db import models
 
 class Team(models.Model):
@@ -6,6 +7,10 @@ class Team(models.Model):
     city = models.CharField(max_length=255)
     prov_state = models.CharField(max_length=255)
     team_score = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        self.name = sub(r'\s+', '_', self.name)
+        super(Team, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return u"%s (%s)" % (self.name, self.uni)
@@ -20,7 +25,20 @@ class Score(models.Model):
     team = models.ForeignKey(Team)
     comp = models.ForeignKey(Competition)
     score = models.IntegerField()
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        #get a list of all the other scores for team
+        other_scores = Score.objects.filter(team=self.team).exclude(id=self.id)
+        
+        sum = self.score
+        for s in other_scores:
+            sum = sum + s.score
+
+        self.team.team_score = sum
+        self.team.save()
+        
+        super(Score, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return u"%s: %s -> %d" % (self.team.name, self.comp, self.score)
